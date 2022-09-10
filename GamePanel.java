7363593b4b3dc47b9975c3ba725cd8a2f633e323
Soare <alexandrusoare02@gmail.com>
2022-09-10 19:49:28 +0300

    -fixed imports,added testing Grid, added randomly generated apple, added timer

--- GamePanel.java
+++ GamePanel.java
@@ -1,12 +1,7 @@
-import java.awt.Color;
-import java.awt.Graphics;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
-import javax.swing.JPanel;
+import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
-import java.util.random.*;
+import java.util.Random;
 
 public class GamePanel extends JPanel implements ActionListener {
 
@@ -27,26 +22,41 @@ public class GamePanel extends JPanel implements ActionListener {
 	Random random;
 
 	GamePanel() {
-		random= new Random();
-		this.setPreferredSize(new Dimension(SCREEN_WIDTH,SCREEN_HEIGHT));
+		random = new Random();
+		this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
 		this.setBackground(Color.black);
 		this.setFocusable(true);
-		this.addKeyListener(new myKeyAdapter());
-		
-		
-		
+		this.addKeyListener(new MyKeyAdapter());
+		startGame();
+
 	}
 
 	public void startGame() {
-
+		newApple();
+		running = true;
+		timer = new Timer(DELAY, this);
+		timer.start();
 	}
 
 	public void paintComponent(Graphics g) {
-
+		super.paintComponent(g);
+		draw(g);
 	}
 
 	public void draw(Graphics g) {
 
+		for (int i = 0; i < SCREEN_HEIGHT / UNIT_SIZE; i++) {
+			g.drawLine(i * UNIT_SIZE, 0, i * UNIT_SIZE, SCREEN_HEIGHT);
+			g.drawLine(0, i * UNIT_SIZE, SCREEN_WIDTH, i * UNIT_SIZE);
+		}
+		g.setColor(Color.red);
+		g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
+
+	}
+
+	public void newApple() {
+		appleX = random.nextInt((SCREEN_WIDTH / UNIT_SIZE)) * UNIT_SIZE;
+		appleY = random.nextInt((SCREEN_HEIGHT / UNIT_SIZE)) * UNIT_SIZE;
 	}
 
 	public void move() {
